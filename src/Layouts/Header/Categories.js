6def153f410a81f9ca7b457a1ca51cb9910a660c
Gurdeep Singh <gurdeepsh25@gmail.com>
2023-03-14 20:18:29 +0530

    cart responsiveness

--- src/Layouts/Header/Categories.js
+++ src/Layouts/Header/Categories.js
@@ -6,6 +6,22 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 import download_the_app from "../../assets/download_the_app.jpg";
 
+const categories = [
+  "Sell",
+  "Amazon miniTV",
+  "Best Sellers",
+  "Mobiles",
+  "Customer Service",
+  "Today's Deals",
+  "Electronics",
+  "Prime",
+  "Amazon Pay",
+  "Fashion",
+  "New Releases",
+  "Home & Kitchen",
+  "Beauty & Personal Care",
+];
+
 function Categories() {
   return (
     <div className="categories">
@@ -13,71 +29,15 @@ function Categories() {
         <MenuIcon />
         <span className="categories__text">All</span>
       </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Sell</span>
+
+      {categories.map((category) => (
+        <div className="categories__nav" key={category}>
+          <div className="categories__option">
+            <span>{category}</span>
+          </div>
         </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Amazon miniTV</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Best Sellers</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Mobiles</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Customer Service</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Today's Deals</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Electronics</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Prime</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Amazon Pay</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Fashion</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>New Releases</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Home & Kitchen</span>
-        </div>
-      </div>
-      <div className="categories__nav">
-        <div className="categories__option">
-          <span>Beauty & Personal Care</span>
-        </div>
-      </div>
+      ))}
+
       <div className="categories__downloadapp">
         <img
           className="categories__downloadappImg"
