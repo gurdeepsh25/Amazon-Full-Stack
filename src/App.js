45eaa45a5f1c85aa070190a2322886e5723d0c56
Gurdeep Singh <gurdeepsh25@gmail.com>
2023-04-08 08:45:14 +0530

    minimal

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ function App() {
                 <Header />
                 <Categories />
                 <HomeAppliances />
+                <Footer />
               </>
             }
           ></Route>
